c9537d63759bf67a9e334e58991239ccd09e4743
Kenneth Endfinger <kaendfinger@gmail.com>
2018-12-31 14:09:06 -0600

    Fix Summon Client

--- bin/summon.dart
+++ bin/summon.dart
@@ -30,26 +30,7 @@ Future<SummonClient> _begin() async {
   var vehicles = await client.listAccountVehicles();
   var vehicle = vehicles.first;
   var summon = await vehicle.summon();
-  summon.onMessage.listen((event) {
-    if (event is SummonAutoparkErrorMessage) {
-      print("[Autopark Error] ${event.errorType}");
-    } else if (event is SummonAutoparkCommandResultMessage) {
-      if (!event.result) {
-        print(
-            "[Autopark Failure] ${event.cmdType} failed: ${event.failureReason}");
-      } else {
-        print("[Autopark Success] ${event.cmdType}");
-      }
-    } else if (event is SummonAutoparkStatusMessage) {
-      print("[Autopark Status] ${event.state}");
-    } else if (event is SummonAutoparkStyleMessage) {
-      print("[Autopark Style] ${event.style}");
-    } else if (event is SummonGoodbyeMessage) {
-      print("[Goodbye] ${event.reason}");
-    } else {
-      print("[Unknown Event] ${event.runtimeType}");
-    }
-  });
+  summon.onMessage.listen(_handleEvent);
   return summon;
 }
 
@@ -57,8 +38,8 @@ main(List<String> args) async {
   client = new TeslaClient(args[0], args[1]);
 
   var summon = await _begin();
-  await for (var line
-      in stdin.transform(const Utf8Decoder()).transform(const LineSplitter())) {
+  stdout.write("> ");
+  await for (var line in stdin.transform(const Utf8Decoder()).transform(const LineSplitter())) {
     if (line == "forward") {
       var vehicle = (await client.listAccountVehicles()).first;
       var state = await vehicle.getAllVehicleState();
@@ -81,5 +62,6 @@ main(List<String> args) async {
       await summon.send(new SummonAutoparkAbortMessage());
       print("[Sent] Abort");
     }
+    stdout.write("> ");
   }
 }
