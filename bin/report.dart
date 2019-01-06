3a33c9a6e5915aa5ef1730d5574909cba71efda2
Kenneth Endfinger <kaendfinger@gmail.com>
2019-01-06 03:49:15 +0000

    Fix Vehicle Report Tool

--- bin/report.dart
+++ bin/report.dart
@@ -9,6 +9,9 @@ TeslaClient client;
 _update() async {
   try {
     for (var vehicle in await client.listAccountVehicles()) {
+      if (vehicle.state == "asleep") {
+        continue;
+      }
       var state = await vehicle.getAllVehicleState();
       var json = new Map<String, dynamic>.from(state.json);
       json["timestamp"] = new DateTime.now().millisecondsSinceEpoch;
@@ -25,5 +28,4 @@ main(List<String> args) async {
   client = new TeslaClient(args[0], args[1]);
 
   await _update();
-  new Timer(const Duration(seconds: 30), _update);
 }
