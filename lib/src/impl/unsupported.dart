ccfc19155b215d6827337f4baabe13365ea9bec9
Kenneth Endfinger <kaendfinger@gmail.com>
2019-01-13 14:30:23 -0600

    Begin work on v0.0.3

--- lib/src/impl/unsupported.dart
+++ lib/src/impl/unsupported.dart
@@ -1,94 +1,92 @@
 library tesla.impl.unsupported;
 
+import 'dart:async';
+
 import '../../tesla.dart';
 
 class TeslaClientImpl implements TeslaClient {
   TeslaClientImpl(String email, String password, TeslaApiEndpoints endpoints) {
-    _fail();
+    throw "This platform is not supported.";
   }
 
   @override
-  String get email => _fail();
+  String get email => throw "This platform is not supported.";
 
   @override
-  String get password => _fail();
+  String get password => throw "This platform is not supported.";
 
   @override
-  Map<String, dynamic> get token => _fail();
+  Map<String, dynamic> get token => throw "This platform is not supported.";
 
   @override
-  bool get isAuthorized => _fail();
+  bool get isAuthorized => throw "This platform is not supported.";
 
   @override
-  TeslaApiEndpoints get endpoints => _fail();
+  TeslaApiEndpoints get endpoints => throw "This platform is not supported.";
 
   @override
   Future<AllVehicleState> getAllVehicleState(int id) {
-    return _fail();
+    throw "This platform is not supported.";
   }
 
   @override
   Future<ChargeState> getChargeState(int id) {
-    return _fail();
+    throw "This platform is not supported.";
   }
 
   @override
   Future<ClimateState> getClimateState(int id) {
-    return _fail();
+    throw "This platform is not supported.";
   }
 
   @override
   Future<DriveState> getDriveState(int id) {
-    return _fail();
+    throw "This platform is not supported.";
   }
 
   @override
   Future<GuiSettings> getGuiSettings(int id) {
-    return _fail();
+    throw "This platform is not supported.";
   }
 
   @override
   Future<Vehicle> getVehicle(int id) {
-    return _fail();
+    throw "This platform is not supported.";
   }
 
   @override
   Future<VehicleConfig> getVehicleConfig(int id) {
-    return _fail();
+    throw "This platform is not supported.";
   }
 
   @override
   Future<List<Vehicle>> listVehicles() {
-    return _fail();
+    throw "This platform is not supported.";
   }
 
   @override
   Future login() {
-    return _fail();
+    throw "This platform is not supported.";
   }
 
   @override
   Future sendVehicleCommand(int id, String command,
       {Map<String, dynamic> params}) {
-    return _fail();
+    throw "This platform is not supported.";
   }
 
   @override
   Future<SummonClient> summon(int vehicleId, String token) {
-    return _fail();
+    throw "This platform is not supported.";
   }
 
   @override
   Future<Vehicle> wake(int id) {
-    return _fail();
+    throw "This platform is not supported.";
   }
 
   @override
   Future close() {
-    return _fail();
-  }
-
-  static dynamic _fail() {
     throw "This platform is not supported.";
   }
 }
