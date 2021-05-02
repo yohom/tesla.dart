d7f49a93844d60f72c43be2278497e2e24b6bd59
yohom <382146139@qq.com>
2021-05-02 08:34:47 +0800

    enhance: 编译错误处理

--- lib/src/vehicle/option_codes.dart
+++ lib/src/vehicle/option_codes.dart
@@ -1,4 +1,4 @@
-import 'package:collection/collection.dart' show IterableExtension;
+part of tesla;
 
 class VehicleOptionCode {
   const VehicleOptionCode(this.code, this.description);
--- lib/tesla.dart
+++ lib/tesla.dart
@@ -19,25 +19,25 @@ library tesla;
 
 import 'dart:async';
 
+import 'package:collection/collection.dart' show IterableExtension;
+
 import 'src/impl/unsupported.dart'
     if (dart.library.html) 'src/impl/browser.dart'
     if (dart.library.io) 'src/impl/io.dart';
 
 part 'src/client.dart';
-part 'src/token.dart';
 part 'src/endpoints.dart';
-
-part 'src/vehicle/summon.dart';
-part 'src/vehicle/option_codes.dart';
-
-part 'src/types/object.dart';
-part 'src/types/vehicle.dart';
+part 'src/token.dart';
 part 'src/types/all_vehicle_state.dart';
 part 'src/types/charge_state.dart';
 part 'src/types/climate_state.dart';
 part 'src/types/drive_state.dart';
 part 'src/types/gui_settings.dart';
 part 'src/types/media_state.dart';
-part 'src/types/speed_limit_mode.dart';
+part 'src/types/object.dart';
 part 'src/types/seat_heater.dart';
+part 'src/types/speed_limit_mode.dart';
+part 'src/types/vehicle.dart';
 part 'src/types/vehicle_state.dart';
+part 'src/vehicle/option_codes.dart';
+part 'src/vehicle/summon.dart';
